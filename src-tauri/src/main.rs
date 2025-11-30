f8cc9d383868f674c6a605a437cb481ce27e0801
Boris Gertsovsky <bg@crazybit.ltd>
2025-11-30 18:21:29 +0100

    feat: Add system tray icon with Show/Quit menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -44,9 +44,42 @@ fn main() {
             // Initialize system tray if available
             #[cfg(desktop)]
             {
-                let handle = app.handle();
-                // Tray icon setup would go here
-                log::info!("Desktop mode initialized");
+                use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+                use tauri::menu::{Menu, MenuItem};
+
+                let show_item = MenuItem::with_id(app, "show", "Show Window", true, None::<&str>)?;
+                let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+                let menu = Menu::with_items(app, &[&show_item, &quit_item])?;
+
+                let _tray = TrayIconBuilder::new()
+                    .icon(app.default_window_icon().unwrap().clone())
+                    .menu(&menu)
+                    .show_menu_on_left_click(false)
+                    .on_menu_event(|app, event| {
+                        match event.id.as_ref() {
+                            "show" => {
+                                if let Some(window) = app.get_webview_window("main") {
+                                    let _ = window.show();
+                                    let _ = window.set_focus();
+                                }
+                            }
+                            "quit" => {
+                                app.exit(0);
+                            }
+                            _ => {}
+                        }
+                    })
+                    .on_tray_icon_event(|tray, event| {
+                        if let TrayIconEvent::Click { button: MouseButton::Left, button_state: MouseButtonState::Up, .. } = event {
+                            if let Some(window) = tray.app_handle().get_webview_window("main") {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                    })
+                    .build(app)?;
+
+                log::info!("System tray initialized");
             }
             
             Ok(())
