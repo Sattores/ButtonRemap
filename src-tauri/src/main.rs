23a99a2085aff6e2ac2e5649f5534d6bc8bd419f
Boris Gertsovsky <bg@crazybit.ltd>
2025-11-30 18:24:21 +0100

    feat: Minimize to tray on window close

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -110,6 +110,12 @@ fn main() {
             commands::open_file_dialog,
             commands::get_app_version,
         ])
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("Error while running USB Configurator");
 }
